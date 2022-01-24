f0e60af8b3b1a44bd9025ea1decc816a530175f3
desantamaria <santamaria.daniel316@gmail.com>
2022-01-24 11:32:47 -0800

    Added discord token to ignored file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const Discord = require('discord.js');
+const key = require('./key.json');
 
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
@@ -40,4 +41,4 @@ client.on('message', message => {
 });
 
 
-client.login('OTM0OTM2ODY4ODI5Njc1NjEw.Ye3Vzw.I-zjHbly-YvkQeYCq4zWC3NEVN0');
+client.login(key.token);
